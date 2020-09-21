e212a2a4b4d01626f139b2895f8dcffe7abaae03
wanglijing <wanglijing@erayt.com>
2020-09-21 17:55:25 +0800

    -新增变换

--- src/index.js
+++ src/index.js
@@ -4,21 +4,34 @@ import { str2rgb } from './util';
 const gl = new WebGL('#webgl');
 gl.setBgColor('#000');
 gl.setPointWidth(10);
+gl.setVertices(new Float32Array([0.0, 0.0, .5, -.5, -.5, -.5]));
+gl.setColor('#a00');
+let start = 0;
+let dir = 1;
+const transform = new Float32Array([
+  1, 0, 0, 0,
+  0, 1, 0, 0,
+  0, 0, 1, 0,
+  start, start, 0, 1,
+]);
 
-requestAnimationFrame(() => {
+function animate() {
   gl.clear();
-  gl.setVertices(new Float32Array([0.0, 0.0, .5, -.5, -.5, -.5]));
-  gl.draw(gl.POINTS, 2);
-  gl.setColor('#a00');
-  gl.setTranslateMartix(new Float32Array([
-    1, 0, 0, 0,
-    0, 1, 0, 0,
-    0, 0, 1, 0,
-    .1, .1, 0, 1,
-  ]))
-})
+  gl.rotate(start);
+  gl.drawPoint(100, 0);
+  start += 0.5;
+  requestAnimationFrame(animate);
+}
+requestAnimationFrame(animate);
+// gl.clear();
+// gl.rotate();
+gl.drawLine(100, 0, 100, 100);
+// gl.drawLine(0, 0, 0, 0);
+// gl.drawLine(0, 0, -100, 100);
 
-gl.draw(gl.TRIANGLES, 2);
+
+
+
 
 
 
